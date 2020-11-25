bb36de9ca657a2d1d1c52bf9aab50596cc4d3d8b
mayuan1 <43050290+mayuan1@users.noreply.github.com>
2020-11-25 11:56:44 +0800

    add age and emotion

--- deepface-race/api.py
+++ deepface-race/api.py
@@ -13,16 +13,25 @@ import numpy as np
 # from deepface.basemodels import VGGFace, OpenFace, Facenet, FbDeepFace, DeepID
 # from deepface.basemodels.DlibResNet import DlibResNet
 
-from src.Race import loadModel
+#from src.Race import loadModel
+import src.Race as Race
+import src.Age as Age
+import src.Emotion as Emotion
 #from src.Emotion import loadModel
 from src.commons.functions import preprocess_face
 
 app = Flask(__name__)
 
-race_model = loadModel()
+race_model = Race.loadModel()
+race_labels = ['asian', 'indian', 'black', 'white', 'middle eastern', 'latino hispanic']
 
-@app.route('/analyze/race', methods=['POST'])
-def analyzeRace():
+age_model = Age.loadModel()
+
+emotion_model = Emotion.loadModel()
+emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+@app.route('/analyze/<name>', methods=['POST'])
+def analyze(name):
 
     tic = time.time()
     req = request.get_json()
@@ -42,7 +51,12 @@ def analyzeRace():
     print("Analyzing image...")
 
     try:
-        resp_obj = predict_race(base64_img, image_type)
+        if name == "race":
+            resp_obj = predict_race(base64_img, image_type)
+        elif name == "age":
+            resp_obj = predict_age(base64_img, image_type)
+        elif name == "emotion":
+            resp_obj = predict_emotion(base64_img, image_type)
 
         toc = time.time()
 
@@ -56,43 +70,80 @@ def analyzeRace():
         return {'error': str(ex)}, 200
 
 
-
-def predict_race(img, img_type, race_probs = True):
+def predict_race(img, img_type, race_probs = 0):
     img_224 = preprocess_face(img = img, img_type = img_type, target_size = (224, 224), grayscale = False, 
         enforce_detection = True, detector_backend = 'opencv') #just emotion model expects grayscale images
     print("img_224 finish!")
     race_predictions = race_model.predict(img_224)[0,:]
     print("prediction finish!")
-    race_labels = ['asian', 'indian', 'black', 'white', 'middle eastern', 'latino hispanic']
-
+    
     sum_of_predictions = race_predictions.sum()
 
-    race_obj = "\"race\": {"
-    for i in range(0, len(race_labels)):
-        race_label = race_labels[i]
-        race_prediction = 100 * race_predictions[i] / sum_of_predictions
-
-        if i > 0: race_obj += ", "
-
-        race_obj += "\"%s\": %s" % (race_label, race_prediction)
-
-    race_obj += "}"
-    dominant_race_obj = "\"dominant_race\": \"%s\"" % (race_labels[np.argmax(race_predictions)])
-    # race_obj += ", \"dominant_race\": \"%s\"" % (race_labels[np.argmax(race_predictions)])
-
-    resp_obj = "{"
-
-    resp_obj += dominant_race_obj
-
-    if race_probs:
-        resp_obj += ","
-        resp_obj += race_obj
+    if race_probs == 0:
+        resp_obj = dict.fromkeys(race_labels, False)
+        resp_obj[race_labels[np.argmax(race_predictions)]] = True
     
-    resp_obj += "}"
+    else:
+
+        resp_obj = "{"
+        for i in range(0, len(race_labels)):
+            race_label = race_labels[i]
+            race_prediction = 100 * race_predictions[i] / sum_of_predictions
+
+            if i > 0: resp_obj += ", "
+
+            resp_obj += "\"%s\": %s" % (race_label, race_prediction)
+        resp_obj += "}"
 
     resp_obj = json.loads(resp_obj)
 
     return resp_obj
+
+
+def predict_age(img, img_type, age_probs = 0):
+    img_224 = preprocess_face(img = img, img_type = img_type, target_size = (224, 224), grayscale = False, 
+        enforce_detection = True, detector_backend = 'opencv') #just emotion model expects grayscale images
+    print("img_224 finish!")
+    age_predictions = age_model.predict(img_224)[0,:]
+    print("prediction finish!")
+    
+    apparent_age = Age.findApparentAge(age_predictions)
+
+    resp_obj = {str(apparent_age): True}
+
+    resp_obj = json.loads(resp_obj)
+
+    return resp_obj
+
+def predict_emotion(img, img_type, emotion_probs = 0):
+    img_224 = preprocess_face(img = img, img_type = img_type, target_size = (224, 224), grayscale = False, 
+        enforce_detection = True, detector_backend = 'opencv') #just emotion model expects grayscale images
+    print("img_224 finish!")
+    emotion_predictions = emotion_model.predict(img_224)[0,:]
+    print("prediction finish!")
+    
+    sum_of_predictions = emotion_predictions.sum()
+
+    if emotion_probs == 0:
+        resp_obj = dict.fromkeys(emotion_labels, False)
+        resp_obj[emotion_labels[np.argmax(emotion_predictions)]] = True
+    
+    else:
+
+        resp_obj = "{"
+        for i in range(0, len(emotion_labels)):
+            emotion_label = emotion_labels[i]
+            emotion_prediction = 100 * emotion_predictions[i] / sum_of_predictions
+
+            if i > 0: resp_obj += ", "
+
+            resp_obj += "\"%s\": %s" % (emotion_label, emotion_prediction)
+        resp_obj += "}"
+
+    resp_obj = json.loads(resp_obj)
+
+    return resp_obj
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
