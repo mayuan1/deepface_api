45809868039dd95154a3d0363ff0a36738d7ec97
mayuan1 <43050290+mayuan1@users.noreply.github.com>
2020-12-01 10:22:18 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,12 @@
 from api import app
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument(
-		'-p', '--port',
-		type=int,
-		default=5431,
-		help='Port of serving api')
-	args = parser.parse_args()
-	app.run(host='0.0.0.0', port=args.port)
+	# parser = argparse.ArgumentParser()
+	# parser.add_argument(
+	# 	'-p', '--port',
+	# 	type=int,
+	# 	default=5431,
+	# 	help='Port of serving api')
+	# args = parser.parse_args()
+	# app.run(host='0.0.0.0', port=args.port)
+    app.run()
