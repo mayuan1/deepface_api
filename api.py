5e9b36849d66d9b5397c1013bbdbcbfc5639fe51
mayuan1 <43050290+mayuan1@users.noreply.github.com>
2020-12-01 10:14:42 +0800

    add wsgi

--- api.py
+++ api.py
@@ -156,14 +156,3 @@ def predict_emotion(img, img_type, emotion_probs = 0):
         resp_obj = json.loads(resp_obj)
 
     return resp_obj
-
-
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument(
-		'-p', '--port',
-		type=int,
-		default=5431,
-		help='Port of serving api')
-	args = parser.parse_args()
-	app.run(host='0.0.0.0', port=args.port)
